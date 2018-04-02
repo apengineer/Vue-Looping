513f84f96ebc661771b0905b0d583ded71950308
Aswathy Prasad <33281453+apengineer@users.noreply.github.com>
2018-04-02 13:06:04 +0200

    Looping inn Vue
    v-for

---
+++ looping/app.js
@@ -0,0 +1,17 @@
+var v_conditionals = new Vue({
+  el: '#v-coditionals',
+  data: {
+    actors : ['Brad Pitt', 'Jonny Depp', 'Silvester'],
+    movies:[
+      {name: 'The Blind Side' , rating: 9},
+      {name: 'Oceans 11', rating: 7.5},
+      {name: '300', rating: 6.5}
+    ]
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+});
